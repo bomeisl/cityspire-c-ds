415dece2a5fd1fd5f88f36bc8557627c588bf42c
johnjdailey <johnjdailey@gmail.com>
2021-02-04 21:15:26 -0500

    Debug SQL in API

--- app/api/location.py
+++ app/api/location.py
@@ -44,7 +44,7 @@ router = APIRouter()
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
-connection = psycopg2.connect(DATABASE_URL)
+connection = psycopg2.connect("postgres://CitySpire:teamwork!@cityspire-c-ds.c2uishzxxikl.us-east-1.rds.amazonaws.com:5432/DB4API")
 
 # Cursor for making SQL queries
 
@@ -104,54 +104,58 @@ async def location_data(location: LocationDataRequest):
     location = location.replace('location=', "")
 
 
-    # Queries for data response
+    # Queries for data response - these same queries work in another notebook with the same code, but not here
 
     #pop_query = """SELECT "2019 Population" FROM CitySpire WHERE "Location" = %s""", [location]
     #rent_query = """SELECT "2019 Rental Rates" FROM CitySpire WHERE "Location" = %s""", [location]
     #walk_query = """SELECT "2019 Walk Score" FROM CitySpire WHERE "Location" = %s""", [location]
     #live_query = """SELECT "2019 Livability Score" FROM CitySpire WHERE "Location" = %s""", [location]
 
-#    cursor.execute("""SELECT "2019 Population" FROM CitySpire WHERE "Location" = %s;""", [location])
-#    pop = cursor.fetchall()
-#    pop = pop[0]
-#
-#    cursor.execute("""SELECT "2019 Rental Rates" FROM CitySpire WHERE "Location" = %s;""", [location])
-#    rent = cursor.fetchall()
-#    rent = rent[0]
-#
-#    cursor.execute("""SELECT "Walk Score" FROM CitySpire WHERE "Location" = %s;""", [location])
-#    walk = cursor.fetchall()
-#    walk = walk[0]
-#
-#    cursor.execute("""SELECT "Livability Score" FROM CitySpire WHERE "Location" = %s;""", [location])
-#    live = cursor.fetchall()
-#    live = live[0]
+    cursor.execute("""SELECT "2019 Population" FROM cityspire WHERE "Location" = %s;""", [location])
+    pop = cursor.fetchall()
+    #pop = pop[0][0] # This is slice slice the tuple value from the list of tuples
 
-######## These queries above and below work in other notebooks, but do not work in this API :(
-
-    cursor.execute("""SELECT CASE WHEN "Location" = %s THEN "2019 Population" END FROM CitySpire;""", [location])
-    pop= cursor.fetchall()
-    pop = pop[0]
-
-    cursor.execute("""SELECT CASE WHEN "Location" = %s THEN "2019 Rental Rates" END FROM CitySpire;""", [location])
+    cursor.execute("""SELECT "2019 Rental Rates" FROM cityspire WHERE "Location" = %s;""", [location])
     rent = cursor.fetchall()
-    rent = rent[0]
+    #rent = rent[0][0] # This is slice slice the tuple value from the list of tuples
 
-    cursor.execute("""SELECT CASE WHEN "Location" = %s THEN "Walk Score" END FROM CitySpire;""", [location])
+    cursor.execute("""SELECT "Walk Score" FROM cityspire WHERE "Location" = %s;""", [location])
     walk = cursor.fetchall()
-    walk = walk[0]
+    #walk = walk[0][0] # This is slice slice the tuple value from the list of tuples
 
-    cursor.execute("""SELECT CASE WHEN "Location" = %s THEN "Livability Score" END FROM CitySpire;;""", [location])
+    cursor.execute("""SELECT "Livability Score" FROM cityspire WHERE "Location" = %s;""", [location])
     live = cursor.fetchall()
-    live = live[0]
+    #live = live[0][0] # This is slice slice the tuple value from the list of tuples
+
+    
+    # Close the cursor and connection
 
     #cursor.close()
     #connection.close()
 
-    return {
-            "city_name": location,
-            "population": 909090,
-            "rent_per_month": 909090,
-            "walk_score": 90,
-            "livability_score": 909090
-        }
+
+    # Trying different return statements for debugging   
+
+    # This returns the static values with no problem, so the POST API works
+
+    #return {
+    #        "city_name": location,
+    #        "population": 287442,
+    #        "rent_per_month": 1597,
+    #        "walk_score": 41,
+    #        "livability_score": 5161
+    #}
+
+    # This returns an empty list (which should contain a tuple of the data queried)
+
+    #return print(pop)
+
+    # This should return the parameters instantiated by fetching the queried data from the DB, but the query returns an empty list
+
+    #return {
+    #    "city_name": location,
+    #    "population": int(pop),
+    #    "rent_per_month": int(rent),
+    #    "walk_score": int(walk),
+    #    "livability_score": int(live)
+    #}
