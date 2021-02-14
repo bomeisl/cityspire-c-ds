d817d662627a2f7a1e08d3794526310e511b5f1c
Kyle Bomeisl <45512800+bomeisl@users.noreply.github.com>
2021-02-13 21:24:50 -0500

    remove all hard coded env variables for native psychopg rec

--- app/db.py
+++ app/db.py
@@ -25,15 +25,12 @@ router = APIRouter()
 # Load environment variables aka secrets from .env
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
-NAME = os.getenv("NAME")
-USER = os.getenv("USER")
-PASS = os.getenv("PASS")
+
 #DATABASE_URL = os.getenv['DATABASE_URL'] # for AWS EB Environment Variable
 
 # Connect to AWS RDS PG DB with FastAPI on Heroku (Hosted on AWS)
 
-connection = psycopg2.connect("dbname=NAME user=USER password=PASS host=DATABASE_URL")
+connection = psycopg2.connect('')
 
 # Cursor for making SQL queries
 
